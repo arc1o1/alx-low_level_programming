91dde960bf6cf988e0d65ec06ffff3c009b42d2b
arc <arc@localhost.localdomain>
2023-02-23 13:20:37 -0500

    more_functions_loops

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 
 /**
@@ -10,9 +10,9 @@ int main(void)
 {
     char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
     c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
+    printf("%c: %d\n", c, _isdigit(c));
     return (0);
 }

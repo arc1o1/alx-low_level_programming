bd40f6d567701c5a837dc2f0a54f0012d95371eb
arc1o1 <124105709+arc1o1@users.noreply.github.com>
2023-02-23 08:49:14 +0000

    function_prints_timetable_from_0_to_n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,22 +4,21 @@
  *
  * Return: always void
  */
-void times_table(void)
+int times_table(int m)
 {
 	int i, n, pTens, pOnes, lineTotal;
 
 	i = 0;
 	pTens = 0;
 	pOnes = 0;
-	if (n < 0 && n > 15)
+	if (m < 0 && m > 15)
 	{
 	    break;
 	}
 	else
 	{
-	    while (i < 10)
+	    while (i <= 15)
 	    {
-		    n = 0;
 		    lineTotal = 0;
 		    while (n <= 15)
 		    {
@@ -33,7 +32,7 @@ void times_table(void)
 			    else if (n > 0)
 				    _putchar(pTens + '0');
 			    _putchar(pOnes + '0');
-			    if (n == 9)
+			    if (n == m)
 				    break;
 			    _putchar(',');
 			    _putchar(' ');

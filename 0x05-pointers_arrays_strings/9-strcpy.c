0ca83e11db63ea62174053347717a615b7b8cc4b
arc1o1 <124105709+arc1o1@users.noreply.github.com>
2023-02-28 18:33:15 +0000

    copy_string_to destination_pointer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *temp = dest;
+	int i;
 
-	while (*src)
-		*dest++ = *src++;
-	return (temp);
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }

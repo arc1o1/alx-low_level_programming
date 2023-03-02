56adf72a4d861186420a01633c85e12c2d6752aa
arc1o1 <124105709+arc1o1@users.noreply.github.com>
2023-03-02 08:47:49 +0000

    Update 3-main.c

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-    char s1[] = "Hel";
-    char s2[] = "Wo";
-
-    printf("%d\n", strcmp(s1, s2));
-    printf("%d\n", strcmp(s2, s1));
-    printf("%d\n", strcmp(s1, s1));
-    return (0);
-}
+	char s1[] = "Hello";
+	char s2[] = "World!";
+	
+	printf("%d\n", strcmp(s1, s2));
+	printf("%d\n", strcmp(s2, s1));
+	printf("%d\n", strcmp(s1, s1));
+	return (0);
+}    

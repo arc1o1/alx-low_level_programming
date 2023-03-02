2e6c13567154f0ea0a4dd0cd9ecf257c5725eca1
arc1o1 <124105709+arc1o1@users.noreply.github.com>
2023-03-02 09:02:28 +0000

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,42 +1,20 @@
+#include "main.h"
+
 /**
- * _strcmp - compares two strings
- *
- * @s1: first string
- * @s2: second string
- *
- * Return: -15 if s1 < s2
- *          0  if s1 = s2
- *          15 if s1 > s2
+ * _strcmp - Compare two strings
+ * @s1: string
+ * @s2: string
+ * Return: negative int if s1 < s2, 0 if matching, and positive int if s1 > s2
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int s1Length = 0, s2Length = 0;
-	int rtrnValNeg = -10;
-	int rtrnValPos = 10;
-	int smallerLength = 0;
+	int i;
 
-	while (*(s1 + s1Length) != '\0')
-		s1Length++;
-	while (*(s2 + s2Length) != '\0')
-		s2Length++;
-
-	smallerLength = (s1Length < s2Length) ? s1Length : s2Length;
-	rtrnValNeg += -(smallerLength);
-	rtrnValPos += smallerLength;
-
-	if (s1Length < s2Length)
-		return (rtrnValNeg);
-	if (s1Length > s2Length)
-		return (rtrnValPos);
-
-	while (*s1) /*same length, so loop through one and check both */
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (*s1 < *s2)
-			return (rtrnValNeg);
-		if (*s1 > *s2)
-			return (rtrnValPos);
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
-	return (0); /* equal */
+	return (0);
 }
